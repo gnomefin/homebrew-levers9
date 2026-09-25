7d2e32c8198c9cfc0c12ac25ab9f27dd6b862db0
levers9 release <hello@uselevers.com>
2026-09-25 15:41:36 +0000

    levers9 0.13.0: bottle

--- Formula/levers9.rb
+++ Formula/levers9.rb
@@ -7,6 +7,11 @@ class Levers9 < Formula
   sha256 "0fd5800ac7b19dbd80eecbb8e8aef61b4d154cc0d5dec84c843d8f487adc3791"
   license "AGPL-3.0-only"
 
+  bottle do
+    root_url "https://github.com/gnomefin/homebrew-levers9/releases/download/levers9-0.13.0"
+    sha256 cellar: :any, arm64_sequoia: "450a6e10b7c8fa09de8bfb6dd2ae3cf5c3c92f2fcecb8dc621c20bbf2b2879de"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
